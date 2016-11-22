cacb1e7c0728dc189405ad13f81f16a2d7351520
Kevin Brown <kevin.s.brown@uconn.edu>
2016-11-22 14:58:51 -0500

    Added ICC calculation to agreement.

--- agreement.py
+++ agreement.py
@@ -4,6 +4,41 @@ Methods for measuring inter-ranker agreement.
 
 from numpy import zeros
 
+def icc(ranks):
+    '''
+    Accepts a list of lists/arrays of ranks and compute the degree of
+    inter-ranker agreement via the Interclass Correlation Coefficient.
+
+    Values of the ICC close to 1.0 indicate unanimity, and those close to
+    zero indicate complete lack of consensus.
+
+    INPUT:
+        ranks: list of lists, required
+            ordering of the ranklists must be the same for all rankers
+
+    OUTPUT:
+        icc : float
+            Interclass Correlation Coefficient
+    '''
+    K = len(ranks)
+    N = len(ranks[0])
+    rij = zeros((N,K))
+    for i in range(N):
+        for j in range(K):
+            rij[i,j] = ranks[j][i]
+    # average item ranks
+    xnbar = rij.sum(axis=1)/K
+    # overall mean
+    xbar = xnbar.sum()/N
+    # compute s^2
+    s2 = (1.0/(K*N))*((rij - xbar)**2).sum()
+    # compute the icc
+    d1 = 1.0/(K-1)
+    t1 = (((xnbar-xbar)**2).sum())/N
+    icc = K*d1*t1/s2 - d1
+    return icc
+
+
 def kendallW(ranks):
     '''
     Accepts a list of lists/arrays of ranks and computes the
